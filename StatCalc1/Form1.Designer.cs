d038af1d80fbd98e74c9897f4955b1ac114102b7
zealot <savichpg@yahoo.com>
2020-08-16 20:10:24 +0300

    Начальная версия

---
+++ StatCalc1/Form1.Designer.cs
@@ -0,0 +1,313 @@
+﻿namespace StatCalc1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.calcButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.valuesList = new System.Windows.Forms.ListBox();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.addButton = new System.Windows.Forms.Button();
+            this.InputField = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ResultField = new System.Windows.Forms.TextBox();
+            this.DeviationList = new System.Windows.Forms.TextBox();
+            this.SortDataList = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.saveButton);
+            this.groupBox1.Controls.Add(this.calcButton);
+            this.groupBox1.Controls.Add(this.clearButton);
+            this.groupBox1.Controls.Add(this.deleteButton);
+            this.groupBox1.Controls.Add(this.valuesList);
+            this.groupBox1.Controls.Add(this.resetButton);
+            this.groupBox1.Controls.Add(this.addButton);
+            this.groupBox1.Controls.Add(this.InputField);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(245, 438);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Данные";
+            // 
+            // saveButton
+            // 
+            this.saveButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.saveButton.Location = new System.Drawing.Point(126, 395);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(113, 37);
+            this.saveButton.TabIndex = 8;
+            this.saveButton.Text = "Сохранить";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // calcButton
+            // 
+            this.calcButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.calcButton.Location = new System.Drawing.Point(10, 395);
+            this.calcButton.Name = "calcButton";
+            this.calcButton.Size = new System.Drawing.Size(110, 37);
+            this.calcButton.TabIndex = 7;
+            this.calcButton.Text = "Вычислить";
+            this.calcButton.UseVisualStyleBackColor = true;
+            this.calcButton.Click += new System.EventHandler(this.calcButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.clearButton.Location = new System.Drawing.Point(127, 352);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(112, 37);
+            this.clearButton.TabIndex = 6;
+            this.clearButton.Text = "Очистить";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.deleteButton.Location = new System.Drawing.Point(10, 352);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(111, 37);
+            this.deleteButton.TabIndex = 5;
+            this.deleteButton.Text = "Удалить";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // valuesList
+            // 
+            this.valuesList.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.valuesList.ForeColor = System.Drawing.Color.DarkCyan;
+            this.valuesList.FormattingEnabled = true;
+            this.valuesList.ItemHeight = 26;
+            this.valuesList.Location = new System.Drawing.Point(9, 105);
+            this.valuesList.Name = "valuesList";
+            this.valuesList.ScrollAlwaysVisible = true;
+            this.valuesList.Size = new System.Drawing.Size(230, 238);
+            this.valuesList.TabIndex = 4;
+            this.valuesList.SelectedIndexChanged += new System.EventHandler(this.valuesList_SelectedIndexChanged);
+            // 
+            // resetButton
+            // 
+            this.resetButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.resetButton.Location = new System.Drawing.Point(127, 63);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(112, 37);
+            this.resetButton.TabIndex = 3;
+            this.resetButton.Text = "Сбросить";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // addButton
+            // 
+            this.addButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.addButton.Location = new System.Drawing.Point(10, 63);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(110, 37);
+            this.addButton.TabIndex = 2;
+            this.addButton.Text = "Добавить";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // InputField
+            // 
+            this.InputField.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.InputField.ForeColor = System.Drawing.Color.Navy;
+            this.InputField.Location = new System.Drawing.Point(106, 27);
+            this.InputField.Name = "InputField";
+            this.InputField.Size = new System.Drawing.Size(133, 32);
+            this.InputField.TabIndex = 1;
+            this.InputField.Text = "0";
+            this.InputField.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.InputField.KeyDown += new System.Windows.Forms.KeyEventHandler(this.InputField_KeyDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(6, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Величина:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.pictureBox1);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.ResultField);
+            this.groupBox2.Controls.Add(this.DeviationList);
+            this.groupBox2.Controls.Add(this.SortDataList);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox2.Location = new System.Drawing.Point(245, 0);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(413, 438);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Результаты";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::StatCalc1.Properties.Resources.logo;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 231);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(395, 201);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(8, 95);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(367, 22);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Список отклонений от среднего значения:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(8, 27);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(297, 22);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Отсортированный список данных:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(8, 177);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(207, 22);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Результат вычислений:";
+            // 
+            // ResultField
+            // 
+            this.ResultField.BackColor = System.Drawing.Color.White;
+            this.ResultField.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ResultField.ForeColor = System.Drawing.Color.Navy;
+            this.ResultField.Location = new System.Drawing.Point(215, 172);
+            this.ResultField.Name = "ResultField";
+            this.ResultField.ReadOnly = true;
+            this.ResultField.Size = new System.Drawing.Size(192, 32);
+            this.ResultField.TabIndex = 4;
+            // 
+            // DeviationList
+            // 
+            this.DeviationList.BackColor = System.Drawing.Color.White;
+            this.DeviationList.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.DeviationList.ForeColor = System.Drawing.Color.Navy;
+            this.DeviationList.Location = new System.Drawing.Point(12, 117);
+            this.DeviationList.Name = "DeviationList";
+            this.DeviationList.ReadOnly = true;
+            this.DeviationList.Size = new System.Drawing.Size(395, 32);
+            this.DeviationList.TabIndex = 3;
+            // 
+            // SortDataList
+            // 
+            this.SortDataList.BackColor = System.Drawing.Color.White;
+            this.SortDataList.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.SortDataList.ForeColor = System.Drawing.Color.Navy;
+            this.SortDataList.Location = new System.Drawing.Point(12, 50);
+            this.SortDataList.Name = "SortDataList";
+            this.SortDataList.ReadOnly = true;
+            this.SortDataList.Size = new System.Drawing.Size(395, 32);
+            this.SortDataList.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(658, 438);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Статистический калькулятор";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox InputField;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListBox valuesList;
+        private System.Windows.Forms.Button resetButton;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button calcButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox ResultField;
+        private System.Windows.Forms.TextBox DeviationList;
+        private System.Windows.Forms.TextBox SortDataList;
+    }
+}
+
---
+++ StatCalc1/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StatCalc1
+{
+    public partial class Form1 : Form
+    {
+        List<double> data;
+        CalcManager manager;
+        public Form1()
+        {
+            InitializeComponent();
+            data = new List<double>();
+            manager = new CalcManager();
+        }
+        private void addValue()
+        {
+            string input = InputField.Text;
+            if (String.IsNullOrEmpty(input))
+            {
+                MessageBox.Show("Вы не ввели исходное значение", "Предупреждение",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                InputField.Focus();
+            }
+            else
+            {
+                try
+                {
+                    double value = Convert.ToDouble(input);
+                    valuesList.Items.Add(value);
+                    data.Add(value);
+                    InputField.Clear();
+                    InputField.Focus();
+                }
+                catch (Exception error)
+                {
+                    MessageBox.Show($"Ошибка при добавлении величины: \n{error.Message}", "Ошибка",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    InputField.Clear();
+                    InputField.Focus();
+                }
+            }
+        }
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            addValue();
+        }
+        private void resetButton_Click(object sender, EventArgs e)
+        {
+            InputField.Clear();
+            InputField.Focus();
+        }
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            int k = valuesList.SelectedIndex;
+            if (k == -1)
+            {
+                MessageBox.Show("Вы не указали удаляемое значение", "Предупреждение",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                valuesList.Items.RemoveAt(k);
+                data.RemoveAt(k);
+            }
+        }
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            valuesList.Items.Clear();
+            data.Clear();
+        }
+        private void calcButton_Click(object sender, EventArgs e)
+        {
+            if (data.Count == 0)
+            {
+                MessageBox.Show("Отсутствуют данные для вычисления", "Предупреждение",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                manager.Data = data;
+                manager.SortData();
+                manager.CalcAverage();
+                manager.BuildDeviations();
+                manager.CalcError();
+                foreach (double x in manager.Data)
+                {
+                    SortDataList.Text += $"{x:F}  _  ";
+                }
+                foreach (double y in manager.Deviations)
+                {
+                    DeviationList.Text += $"{y:F}  _  ";
+                }
+                ResultField.Text = $"{manager.Average:F} +- {manager.MeasureError:F}";
+            }
+        }
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void valuesList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void InputField_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                addValue();
+            }
+        }
+    }
+}
